6242bfc5501b298f6bb093acc25b512ab8f8cfd6
Stefan Freudenreich <stefan.freudenreich@agido.com>
2017-11-15 08:01:18 +0100

    Compare all actions

--- src/asserts/utils/assertDispatchedActions.js
+++ src/asserts/utils/assertDispatchedActions.js
@@ -1,18 +1,13 @@
-import findIndex from 'lodash.findindex';
-import { notDispatchedActionError } from '../errors/notDispatchedActionError';
+import isEqual from 'lodash.isEqual';
+import {notDispatchedActionError} from '../errors/notDispatchedActionError';
 
 function assertDispatchedActions(dispatched, expected) {
-  const availableActions = dispatched.slice();
+    const availableActions = dispatched.slice();
 
-  for (let indexInExpected = 0; indexInExpected < expected.length; indexInExpected++) {
-    const indexInAvailable = findIndex(availableActions, expected[indexInExpected]);
-
-    if (indexInAvailable !== -1) {
-      availableActions.splice(indexInAvailable, 1);
-    } else {
-      throw notDispatchedActionError(dispatched, expected, expected[indexInExpected]);
+    for (let index = 0; index < Math.max(availableActions.length, expected.length); index++) {
+        if (!isEqual(availableActions[index], expected[index]))
+            throw notDispatchedActionError(dispatched, expected, expected[index]);
     }
-  }
 }
 
-export { assertDispatchedActions };
+export {assertDispatchedActions};
